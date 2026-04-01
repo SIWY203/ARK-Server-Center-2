e73223859bd07234e6bfebff5f75ec432550506b
SIWY203 <cefalaspis12345@gmail.com>
2026-04-01 03:23:17 +0200

    created Clusters, and Cluster Servers models

---
+++ ArkCluster.cs
@@ -0,0 +1,42 @@
+﻿namespace Ark_Server_Center;
+using static MessageManager;
+
+
+
+public class ArkCluster
+{
+    public string Name { get; private set; }
+    public List<ClusterServer> Servers { get; private set; } = new List<ClusterServer>();
+
+
+    public ArkCluster(string name, List<ClusterServer>? servers = null)
+    {
+        Name = name;
+        Servers = servers ?? new List<ClusterServer>(); // jeśli nie podano serwerów, nowa pusta lista
+    }
+
+
+    public void AddServer(string map, int port)
+    {
+        Servers.Add(new ClusterServer(map, port));
+        Success($"Dodano serwer do klastra '{Name}': Map = {map}, Port = {port}");
+    }
+
+
+    public void RemoveServer(string map, int port)
+    {
+        var serverToRemove = Servers.FirstOrDefault(s => s.Map == map && s.Port == port);
+        if (serverToRemove != null)
+        {
+            Servers.Remove(serverToRemove);
+            Success($"Usunięto serwer z klastra '{Name}': Map = {map}, Port = {port}");
+        }
+        else
+        {
+            Warn($"Nie znaleziono serwera do usunięcia w klastrze '{Name}': Map = {map}, Port = {port}");
+        }
+    }
+
+
+}
+
---
+++ ClusterServer.cs
@@ -0,0 +1,18 @@
+﻿namespace Ark_Server_Center;
+using static MessageManager;
+
+
+public class ClusterServer
+{
+    public string Map { get; private set; }
+    public int Port { get; private set; }
+
+
+    public ClusterServer(string map, int port)
+    {
+        Map = map;
+        Port = port;
+    }
+
+
+}
