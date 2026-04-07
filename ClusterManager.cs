e9ac7351f07db579b9a768561a9798ae8c6ba5ce
SIWY203 <cefalaspis12345@gmail.com>
2026-04-07 23:55:42 +0200

    created ClusterManager.cs to - manage Session/activeCluster and activeServer. - manage cluster and server selecting - manage json config file currently ClusterCreator() is in progress...

---
+++ ClusterManager.cs
@@ -0,0 +1,149 @@
+﻿namespace Ark_Server_Center;
+
+using System.ComponentModel.Design;
+using System.Text.Json;
+using static MessageManager;
+
+
+public static class ClusterManager
+{
+    public static ArkCluster? ActiveCluster { get; private set; }
+    public static ClusterServer? ActiveServer { get; private set; }
+    public static bool IsServerSelected => ActiveServer != null;
+
+
+    private static string configFile = "clusters.json";
+    private static List<ArkCluster> clusters = new();
+
+
+
+    public static void LoadClusters()
+    {
+        if (!File.Exists(configFile))
+        {
+            clusters = new List<ArkCluster>();
+            SaveClusters(); // tworzenie pliku
+            ClusterCreator();
+            return;
+        }
+
+        if (clusters.Count < 1) ClusterCreator();
+
+        try
+        {
+            string jsonString = File.ReadAllText(configFile);
+            clusters = JsonSerializer.Deserialize<List<ArkCluster>>(jsonString) ?? new();
+        }
+        catch (Exception ex)
+        {
+            Error($"Błąd podczas wczytywania pliku:\n{ex.Message}");
+            clusters = new List<ArkCluster>();
+        }
+    }
+
+
+    public static void SaveClusters()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true }; // ładny format
+        string jsonString = JsonSerializer.Serialize(clusters, options);
+        File.WriteAllText(configFile, jsonString);
+    }
+
+
+    public static void ClusterCreator()
+    {
+        Success("Kreator Klastrów");
+        End(); return;
+    }
+
+
+    public static void RequireServerSelection()
+    {
+        while (!IsServerSelected)
+        {
+            SelectCluster();
+
+            if (ActiveCluster != null)
+            {
+                SelectClusterServer(ActiveCluster);
+            }
+            else
+            {
+                // wybrano [Q] Wyjdź
+                Environment.Exit(0);
+            }
+        }
+    }
+
+
+    public static void SelectCluster()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("========= ARK SERVER CENTER =========");
+            Console.WriteLine("Wybierz klaster do zarządzania:\n");
+
+            for (int i = 0; i < clusters.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] Klaster: {clusters[i].Name} ({clusters[i].Servers.Count} serwerów)");
+            }
+            Console.WriteLine("[Q] Wyjdź");
+            Console.Write("\nWybierz: ");
+
+            string? input = Console.ReadLine()?.ToUpper();
+            if (input == "Q")
+            {
+                ActiveCluster = null;
+                break;
+            }
+
+            if (int.TryParse(input, out int choice) && choice - 1 >= 0 && choice - 1 < clusters.Count)
+            {
+                ActiveCluster = clusters[choice - 1];
+                break;
+            }
+        }
+    }
+
+    public static void SelectClusterServer(ArkCluster cluster)
+    {
+        if (cluster == null)
+        {
+            Console.WriteLine("Nie wybrano klastra!");
+            End();
+            return;
+        }
+
+        bool success = false;
+        while (!success)
+        {
+            Console.Clear();
+            Console.WriteLine($"--- KLASTER: {cluster.Name} ---");
+
+            for (int i = 0; i < cluster.Servers.Count; i++)
+            {
+                var s = cluster.Servers[i];
+                string status = SafetyChecker.IsServerRunningOnPort(s.Port) ? "[ONLINE]" : "[OFFLINE]";
+                Console.WriteLine($"[{i + 1}] {s.Map} (Port: {s.Port}) - {status}");
+            }
+            Console.WriteLine("[Q] Wróć");
+            Console.Write("\nWybierz: ");
+
+            string? input = Console.ReadLine()?.ToUpper();
+            if (input == "Q") break;
+
+            if (int.TryParse(input, out int choice) && choice - 1 >= 0 && choice - 1 < cluster.Servers.Count)
+            {
+                Console.WriteLine(cluster.Servers[choice - 1].Map);
+                Console.ReadLine();
+                ClusterServer server = cluster.Servers[choice - 1];
+
+                success = true;
+                ActiveServer = server;
+            }
+        }
+    }
+
+
+}
