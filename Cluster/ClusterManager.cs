fe8f5f39fcf5e340aac7ee7e761112fc63317517
SIWY203 <cefalaspis12345@gmail.com>
2026-04-09 04:45:57 +0200

    - creating the ClusterCreator() - fixed SelectCluster(), now it starts ClusterCreator()

--- Cluster/ClusterManager.cs
+++ Cluster/ClusterManager.cs
@@ -48,10 +48,134 @@ public static class ClusterManager
 
     public static void ClusterCreator()
     {
-        Success("Kreator Klastrów");
+        
+        string clusterName = string.Empty;
+        string clusterPath = RootPath.Value;
+        ArkCluster newCluster = new ArkCluster(clusterName, clusterPath);
+
+        string mapName = string.Empty;
+        int? serverPort = null;
+        ClusterServer newServer = new ClusterServer(mapName, serverPort ?? 0, clusterPath);
+
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine($"\n======== Kreator Klastrów ========\n");
+            Console.Write("Podaj Nazwę klastra: ");
+            string input = Console.ReadLine()?.Trim() ?? string.Empty;
+
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            bool hasInvalidChars = input.Any(c => invalidChars.Contains(c));
+            if (hasInvalidChars)
+            {
+                Console.Clear();
+                Error(
+                    $"Wpisano niedozwolone znaki!\n" +
+                    $"Niedozwolone są: {string.Join(" ", invalidChars.Where(c => !char.IsControl(c)).ToArray())}"); // filtr niewidocznych znaków
+                End(); continue;
+            }
+            else if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.Clear();
+                Error("Wpis nie może być pusty!");
+                End(); continue;
+            }
+            else
+            {
+                clusterName = input;
+                break;
+            }
+        }
+
+        if (string.IsNullOrWhiteSpace(RootPath.Value))
+        {
+            while (string.IsNullOrWhiteSpace(RootPath.Value))
+            {
+                Console.Clear();
+                Console.WriteLine($"\n======== Kreator Klastrów ========\n");
+                Warn("Jeszcze nie ustawiono domyślnej ścieżki!\n");
+                RootPath.Value = FileManager.GetFolderPath() ?? string.Empty;
+            }
+            Console.Clear();
+            Success("Scieżka główna została zapisana!");
+            End();
+        }
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine($"\n======== Kreator Klastrów ========\n");
+            Console.WriteLine($"Domyślna ścieżka: {RootPath.Value}\n");
+            Console.WriteLine(
+                "[1] Zastosuj\n" +
+                "[2] Ustaw inną\n" +
+                "[3] Anuluj\n");
+
+            Console.Write("Wybierz: ");
+            string input = Console.ReadLine()?.Trim() ?? string.Empty;
+
+            if (input == "1")
+            {
+                clusterPath = RootPath.Value;
+                Console.Clear();
+                Success("Zapisano ścieżkę!");
+                End(); break;
+            }
+
+            else if (input == "2")
+            {
+                string? newPath = FileManager.GetFolderPath();
+                if (!string.IsNullOrWhiteSpace(newPath))
+                {
+                    clusterPath = newPath;
+                    Console.Clear();
+                    Success("Zapisano ścieżkę!");
+                    End(); break;
+                }
+            }
+
+            else if (input == "3")
+            {
+                Console.Clear();
+                Console.WriteLine("Anulowano tworzenie klastra.");
+                End(); return;
+            }
+
+            else
+            {
+                Console.Clear();
+                Error("Nieprawidłowy wybór, spróbuj ponownie.");
+                End();
+            }
+            
+        }
+
+
+        // dodać enum map i ręczne wpisywanie niestandardowych map
+        // dodawanie serwerów do klastra
+        // algorytm przypisywania portów i opcja zmiany z info o błędach
+        // poprawić cały ClusterCreator() by był user-friendly
+        // 
+        // w SelectCluster() ewentualnie usunąć End() po ClusterCreator()
 
 
 
+        Console.Clear();
+        Console.WriteLine($"\n======== Kreator Klastrów ========\n");
+        Console.WriteLine();
+        Console.WriteLine($"Nazwa klastra: {clusterName}\n");
+        Console.WriteLine($"Serwery:\n");
+        for (int i = 0; i < newCluster.Servers.Count; i++)
+        {
+            var server = newCluster.Servers[i];
+            string status = SafetyChecker.IsServerRunningOnPort(server.Port) ? "[ONLINE]" : "[OFFLINE]";
+            Console.WriteLine($" - Mapa: {server.Map} (Port: {server.Port}) - {status}");
+        }
+
+
+
+        ActiveCluster = newCluster;
         End(); return;
     }
 
@@ -60,8 +184,7 @@ public static class ClusterManager
     {
         if (IsServerSelected)
         {
-            // Zapamiętanie stanu, aby nie zostawić nulla
-            ArkCluster? cluster = ActiveCluster;
+            ArkCluster? cluster = ActiveCluster; // zapis stanu, by nie było nulla
             SelectCluster();
             if (ActiveCluster != null) SelectClusterServer(ActiveCluster);
             else ActiveCluster = cluster;
@@ -83,11 +206,12 @@ public static class ClusterManager
             Console.Clear();
             Console.WriteLine("========= ARK SERVER CENTER =========");
             Console.WriteLine("Wybierz klaster do zarządzania:\n");
-
-            for (int i = 0; i < clusters.Count; i++)
+            int i;
+            for (i = 0; i < clusters.Count; i++)
             {
                 Console.WriteLine($"[{i + 1}] Klaster: {clusters[i].Name} ({clusters[i].Servers.Count} serwerów)");
             }
+            Console.WriteLine($"[{i + 1}] Nowy klaster");
             Console.WriteLine("[Q] Wyjdź");
             Console.Write("\nWybierz: ");
 
@@ -95,6 +219,13 @@ public static class ClusterManager
             if (input == "Q")
             {
                 ActiveCluster = null;
+                break;
+            }
+
+            if (input == $"{i + 1}")
+            {
+                ClusterCreator();
+                End();
                 break;
             }
 
