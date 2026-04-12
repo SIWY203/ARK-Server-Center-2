4ef57bd95c229e9c9653dd4a93f29f12b9a54023
SIWY203 <cefalaspis12345@gmail.com>
2026-04-12 22:49:47 +0200

    - ActiveCluster and ActiveServer are no longer read-only - code adapted to changes in other classes - added CreateServer() - added DeleteCluster(), and DeleteServer() - replaced RequireServerSelection() with SelectClusterAndServer() - UI fixes - implemented new CreateServer, DeleteServer and DeleteCluster methods
    'configFile' and 'clusters' are now in accordance with art: renamed:
    - renamed to _configFile, and _clusters
    - now they are private
    - added new public ReadOnly list 'Clusters'

--- Cluster/ClusterManager.cs
+++ Cluster/ClusterManager.cs
@@ -1,89 +1,70 @@
 ﻿namespace ArkServerCenter.Cluster;
-
-using System.Security.Cryptography;
 using System.Text.Json;
 using static MessageManager;
 
 
 public static class ClusterManager
 {
-    public static Cluster? ActiveCluster { get; private set; }
-    public static ClusterServer? ActiveServer { get; private set; }
+    public static Cluster? ActiveCluster { get; set; }
+    public static ClusterServer? ActiveServer { get; set; }
     public static bool IsServerSelected => ActiveServer != null;
 
 
-    public static string configFile = "clusters.json";
-    public static List<Cluster> clusters = new();
-
+    private static string _configFile = "clusters.json";
+    private static List<Cluster> _clusters = new();
+    public static IReadOnlyList<Cluster> Clusters => _clusters;
 
 
     public static void LoadClusters()
     {
-        if (!File.Exists(configFile))
+        if (!File.Exists(_configFile))
         {
-            clusters = new List<Cluster>();
+            _clusters = new List<Cluster>();
             SaveClusters(); // tworzenie pliku
         }
 
         try
         {
-            string jsonString = File.ReadAllText(configFile);
-            clusters = JsonSerializer.Deserialize<List<Cluster>>(jsonString) ?? new();
+            string jsonString = File.ReadAllText(_configFile);
+            _clusters = JsonSerializer.Deserialize<List<Cluster>>(jsonString) ?? new();
         }
         catch (Exception ex)
         {
             Error($"Błąd podczas wczytywania pliku:\n{ex.Message}");
-            clusters = new List<Cluster>();
+            _clusters = new List<Cluster>();
         }
 
-        if (clusters.Count < 1) ClusterCreator();
+        if (_clusters.Count < 1) CreateCluster();
     }
-
 
 
     public static void SaveClusters()
     {
         var options = new JsonSerializerOptions { WriteIndented = true }; // ładny format
-        string jsonString = JsonSerializer.Serialize(clusters, options);
-        File.WriteAllText(configFile, jsonString);
+        string jsonString = JsonSerializer.Serialize(_clusters, options);
+        File.WriteAllText(_configFile, jsonString);
     }
 
 
-    // -----------------------------
-    //
-    // TO DO
-    // - weryfikacja unikalności nazw klastrów
-    // - rozwiązać problem, gdy zmieni się RootPath, trzeba zaktualizować ścieżki w serwerach,
-    //   a potem zaktualizować jsona, by nie było błędów przy wczytywaniu
-    // - czy aby ClusterCreator() nie powinien być w Cluster.cs? Raczej nie?
-    // - nowa metoda UpdateClusterServer(), bez tego nie ma 'Saved' itd
-    // - Oprócz ClusterCreator() dodać ServerCreator()
-    // - dodać nowe RemoveCluster(), RemoveServer()
-    // - dodać AddClusterFromFiles() - dodaje do pliku json z obcego folderu
-    // 
-    // -----------------------------
-
-
-    public static void ClusterCreator()
+    public static void CreateCluster()
     {
-        string? clusterName = ClusterName.AskForClusterName();
+        string clusterPath = RootPath.Value;
+
+        string? clusterName = ClusterName.AskForClusterName("\n========== Kreator Klastrów ==========\n");
         if (clusterName == null) return; // "Anuluj"
 
-        string? clusterPath = Path.Combine(ClusterPath.AskForClusterPath()!, clusterName);
-        if (clusterPath == null) return;
-
-        string? mapName = ClusterServerMap.AskForClusterServerMap();
+        string? mapName = ServerMap.AskForServerMap("\n========== Kreator Klastrów ==========\n");
         if (mapName == null) return;
 
-        int? serverPort = ClusterServerPort.AskForClusterServerPort();
+        int? serverPort = ServerPort.AskForServerPort("\n========== Kreator Klastrów ==========\n");
         if (serverPort == null) return;
 
 
-        Cluster newCluster = new Cluster(clusterName, clusterPath);
-        ClusterServer newServer = new ClusterServer(mapName, serverPort ?? -1, clusterPath);
+        Cluster newCluster = new Cluster(clusterName);
+        ClusterServer newServer = new ClusterServer(mapName, serverPort.Value, clusterName);
         newCluster.Servers.Add(newServer);
 
-        clusters.Add(newCluster);   // zapis do listy
+        _clusters.Add(newCluster);  // zapis do listy
         ActiveCluster = newCluster; // zapis do sesji
         SaveClusters();             // zapis do json
 
@@ -91,45 +72,300 @@ public static class ClusterManager
         Directory.CreateDirectory(newCluster.ClusterDataPath);
         Directory.CreateDirectory(newServer.ServerRootPath);
 
+        CreateServer();
+    }
+
+
+    public static void DeleteCluster()
+    {
+        while (true)
+        {
+            if (_clusters.Count == 0)
+            {
+                Console.Clear();
+                Error("Obecnie nie ma żadnych klastrów!");
+                End(); return;
+            }
+
+            Console.Clear();
+            Console.WriteLine($"\nKtóry klaster usunąć?\n");
+
+            int i;
+            for (i = 0; i < _clusters.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] Klaster: {_clusters[i].Name} ({_clusters[i].Servers.Count} serwerów)");
+            }
+            Console.WriteLine("[Q] Wyjdź");
+            Console.Write("\nWybierz: ");
+
+            string? input = Console.ReadLine()?.ToUpper();
+            if (input == "Q")
+            {
+                ActiveCluster = null;
+                return;
+            }
+
+            if (int.TryParse(input, out int choice) && choice - 1 >= 0 && choice - 1 < _clusters.Count)
+            {
+                ActiveCluster = _clusters[choice - 1];
+                break;
+            }
+
+            if (ActiveCluster == null)
+            {
+                Console.Clear();
+                Error("Nie wybrano klastra!");
+                End(); continue;
+            }
+        }
+
+        while (true)
+        {
+            Console.Clear();
+            Warn(
+                $"UWAGA! Usunięcie klastra spowoduje wykasowanie z dysku\n" +
+                $"danych klastra {ActiveCluster?.Name} wraz z jego serwerami\n");
+            Console.WriteLine(
+                "[1] Tak, usuń\n" +
+                "[Q] Nie usuwaj!");
+            Console.Write("\nWybierz: ");
+            string input = Console.ReadLine()?.Trim() ?? "";
+
+            if (input != "1")
+            {
+                Console.Clear();
+                Console.WriteLine("Anulowano usuwanie klastra.");
+                End(); return;
+            }
+
+            else
+            {
+                Console.Clear();
+                Warn($"Czy napewno chcesz usunąć klaster {ActiveCluster?.Name}?\n");
+                Console.WriteLine(
+                    "[1] Tak, usuń\n" +
+                    "[Q] Nie usuwaj!");
+                Console.Write("\nWybierz: ");
+                string confirm = Console.ReadLine()?.Trim() ?? "";
+
+                if (confirm != "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Anulowano usuwanie klastra.");
+                    End(); return;
+                }
+
+                _clusters.Remove(ActiveCluster!);
+                SaveClusters(); // lista i json
+
+                string pathToClean = ActiveCluster?.ClusterRootPath ?? string.Empty;
+                if (!string.IsNullOrWhiteSpace(pathToClean) && Directory.Exists(pathToClean))
+                {
+                    Directory.Delete(pathToClean, true);
+                }
+
+                ActiveCluster = null;
+                ActiveServer = null;
+
+                Console.Clear();
+                Success("Klaster został usunięty.");
+                End(); return;
+            }
+        }
+    }
+
+
+    public static void CreateServer()
+    {
+        string? map = string.Empty;
+        int? port = null;
+        while (true)
+        {
+            if (ActiveCluster == null)
+            {
+                Error("Nie wybrano klastra!");
+                End(); return;
+            }
+
+            Console.Clear();
+            ActiveCluster.ShowClusterInfo();
+
+            Console.WriteLine(
+                    "\n" +
+                    "[1] Dodaj serwer\n" +
+                    "[Q] Wyjdź"
+                );
+            Console.Write("\nWybierz: ");
+            string input = Console.ReadLine()?.Trim() ?? "";
+
+            if (input.ToUpper() == "Q") return;
+
+            else if (input == "1")
+            {
+                map = ServerMap.AskForServerMap("\n========== Kreator Serwerów ==========\n");
+                if (map == null) continue;
+
+                port = ServerPort.AskForServerPort("\n========== Kreator Serwerów ==========\n");
+                if (port == null) continue;
+
+                ClusterServer server = new ClusterServer(map, port.Value, ActiveCluster.Name);
+                ActiveCluster.Servers.Add(server);
+
+                Directory.CreateDirectory(server.ServerRootPath);
+                SaveClusters();
+            }
+
+            else
+            {
+                Console.Clear();
+                Error("Nieprawidłowy wybór!");
+                End();
+            }
+        }
+        
+    }
+
+
+    public static void DeleteServer()
+    {
+        if (ActiveCluster == null)
+        {
+            Console.Clear();
+            Error("Nie wybrano klastra!");
+            End(); return;
+        }
+
+        string? input;
+        while (true)
+        {
+            if (ActiveCluster?.Servers.Count == 0)
+            {
+                Console.Clear();
+                Error("Tu nie ma żadnych serwerów!");
+                End(); return;
+            }
+
+            Console.Clear();
+            Console.WriteLine($"\nKtóry serwer usunąć?\n");
+            int i;
+            for (i = 0; i < ActiveCluster?.Servers.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] Serwer: {ActiveCluster.Servers[i].Map} ({ActiveCluster.Servers[i].Port})");
+            }
+            Console.WriteLine("[Q] Wyjdź");
+            Console.Write("\nWybierz: ");
+
+            input = Console.ReadLine()?.ToUpper();
+            if (input == "Q")
+            {
+                ActiveServer = null;
+                return;
+            }
+
+            if (int.TryParse(input, out int choice) && choice - 1 >= 0 && choice - 1 < ActiveCluster?.Servers.Count)
+            {
+                ActiveServer = ActiveCluster.Servers[choice - 1];
+                break;
+            }
+
+            else 
+            {
+                Console.Clear();
+                Error("Nieprawidłowy wybór!");
+                End();
+            }
+        }
+
+        if (ActiveServer == null)
+        {
+            Console.Clear();
+            Error("Nie wybrano serwera!");
+            End(); return;
+        }
 
         Console.Clear();
-        Console.WriteLine($"\n======== Kreator Klastrów ========\n");
-        Console.WriteLine($"Nazwa klastra: {clusterName}");
-        Console.WriteLine($"Lokalizacja: {clusterPath}");
-        Console.WriteLine($"Serwery w klastrze ({newCluster.Servers.Count}):");
+        Warn(
+            $"UWAGA! Usunięcie serwera spowoduje wykasowanie z dysku\n" +
+            $"danych serwera {ActiveServer?.Map} ({ActiveServer?.Port}) wraz z jego plikami\n");
+        Console.WriteLine(
+            "[1] Tak, usuń\n" +
+            "[Q] Nie usuwaj!");
+        Console.Write("\nWybierz: ");
+        input = Console.ReadLine()?.Trim() ?? "";
 
-        if (newCluster.Servers.Count == 0) Console.WriteLine(" ► brak serwerów");
-
-        for (int i = 0; i < newCluster.Servers.Count; i++)
+        if (input != "1")
         {
-            var server = newCluster.Servers[i];
-            string status = SafetyChecker.IsServerRunningOnPort(server.Port) ? "[ONLINE]" : "[OFFLINE]";
-            Console.WriteLine($" ► Mapa: {server.Map} (Port: {server.Port}) - {status}");
+            Console.Clear();
+            Console.WriteLine("Anulowano usuwanie serwera.");
+            End(); return;
         }
 
-        End(); return;
+        else
+        {
+            Console.Clear();
+            Warn($"Czy napewno chcesz usunąć serwer {ActiveServer?.Map} ({ActiveServer?.Port})?\n");
+            Console.WriteLine(
+                "[1] Tak, usuń\n" +
+                "[Q] Nie usuwaj!"); 
+            Console.Write("\nWybierz: ");
+            string confirm = Console.ReadLine()?.Trim() ?? "";
+
+            if (confirm != "1")
+            {
+                Console.Clear();
+                Console.WriteLine("Anulowano usuwanie serwera.");
+                End(); return;
+            }
+
+            ActiveCluster.Servers.Remove(ActiveServer!);
+            SaveClusters(); // lista i json
+
+            string pathToClean = ActiveServer?.ServerRootPath ?? string.Empty;
+            if (!string.IsNullOrWhiteSpace(pathToClean) && Directory.Exists(pathToClean))
+            {
+                Directory.Delete(pathToClean, true);
+            }
+
+            ActiveCluster = null;
+            ActiveServer = null;
+
+            Console.Clear();
+            Success("Serwer został usunięty.");
+            End(); return;
+        }
     }
 
 
-
-    public static void RequireServerSelection()
+    public static void SelectClusterAndServer()
     {
-        if (IsServerSelected)
+        ActiveServer = null;
+        while (ActiveServer == null)
         {
-            Cluster? cluster = ActiveCluster; // zapis stanu, by nie było nulla
             SelectCluster();
-            if (ActiveCluster != null) SelectClusterServer(ActiveCluster);
-            else ActiveCluster = cluster;
-        }
 
-        while (!IsServerSelected)
-        {
-            SelectCluster();
-            if (ActiveCluster != null) SelectClusterServer(ActiveCluster);
-            else Environment.Exit(0);
+            if (ActiveCluster == null) Environment.Exit(0);
+            else SelectClusterServer(ActiveCluster);
         }
     }
 
+
+    //public static void RequireServerSelection()
+    //{
+    //    if (IsServerSelected)
+    //    {
+    //        Cluster? cluster = ActiveCluster; // zapis stanu, by nie było nulla
+    //        SelectCluster();
+    //        if (ActiveCluster != null) SelectClusterServer(ActiveCluster);
+    //        else ActiveCluster = cluster;
+    //    }
+
+    //    while (!IsServerSelected)
+    //    {
+    //        SelectCluster();
+    //        if (ActiveCluster != null) SelectClusterServer(ActiveCluster);
+    //        else Environment.Exit(0);
+    //    }
+    //}
 
 
     public static void SelectCluster()
@@ -140,11 +376,12 @@ public static class ClusterManager
             Console.WriteLine("========= ARK SERVER CENTER =========");
             Console.WriteLine("Wybierz klaster do zarządzania:\n");
             int i;
-            for (i = 0; i < clusters.Count; i++)
+            for (i = 0; i < _clusters.Count; i++)
             {
-                Console.WriteLine($"[{i + 1}] Klaster: {clusters[i].Name} ({clusters[i].Servers.Count} serwerów)");
+                Console.WriteLine($"[{i + 1}] Klaster: {_clusters[i].Name} ({_clusters[i].Servers.Count} serwerów)");
             }
             Console.WriteLine($"[{i + 1}] Nowy klaster");
+            Console.WriteLine($"[{i + 2}] Usuń klaster");
             Console.WriteLine("[Q] Wyjdź");
             Console.Write("\nWybierz: ");
 
@@ -152,18 +389,24 @@ public static class ClusterManager
             if (input == "Q")
             {
                 ActiveCluster = null;
-                break;
+                break; // exit program
             }
 
             if (input == $"{i + 1}")
             {
-                ClusterCreator();
+                CreateCluster();
                 continue;
             }
 
-            if (int.TryParse(input, out int choice) && choice - 1 >= 0 && choice - 1 < clusters.Count)
+            if (input == $"{i + 2}")
             {
-                ActiveCluster = clusters[choice - 1];
+                DeleteCluster();
+                continue;
+            }
+
+            if (int.TryParse(input, out int choice) && choice - 1 >= 0 && choice - 1 < _clusters.Count)
+            {
+                ActiveCluster = _clusters[choice - 1];
                 break;
             }
         }
@@ -184,19 +427,34 @@ public static class ClusterManager
         while (!success)
         {
             Console.Clear();
-            Console.WriteLine($"--- KLASTER: {cluster.Name} ---");
+            Console.WriteLine($"------ KLASTER: {cluster.Name} ------");
 
-            for (int i = 0; i < cluster.Servers.Count; i++)
+            int i;
+            for (i = 0; i < cluster.Servers.Count; i++)
             {
                 var s = cluster.Servers[i];
                 string status = SafetyChecker.IsServerRunningOnPort(s.Port) ? "[ONLINE]" : "[OFFLINE]";
                 Console.WriteLine($"[{i + 1}] {s.Map} (Port: {s.Port}) - {status}");
             }
+            Console.WriteLine($"[{i + 1}] Nowy serwer");
+            Console.WriteLine($"[{i + 2}] Usuń serwer");
             Console.WriteLine("[Q] Wróć");
             Console.Write("\nWybierz: ");
 
             string? input = Console.ReadLine()?.ToUpper();
             if (input == "Q") break;
+
+            if (input == $"{i + 1}")
+            {
+                CreateServer();
+                continue;
+            }
+
+            if (input == $"{i + 2}")
+            {
+                DeleteServer();
+                continue;
+            }
 
             if (int.TryParse(input, out int choice) && choice - 1 >= 0 && choice - 1 < cluster.Servers.Count)
             {
