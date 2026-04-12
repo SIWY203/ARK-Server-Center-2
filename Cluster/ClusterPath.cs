703f3d77c014d273d30c1ba172030435ed399518
SIWY203 <cefalaspis12345@gmail.com>
2026-04-12 22:35:36 +0200

    - all commented, because this code is useless now - later it can be used to custom cluster path

--- Cluster/ClusterPath.cs
+++ Cluster/ClusterPath.cs
@@ -1,59 +1,66 @@
-﻿namespace ArkServerCenter.Cluster;
-using static MessageManager;
+﻿
+// ==================================
+// Funkcja niestandardowej ścieżki
+// obecnie nie jest używana
+// ==================================
 
 
-public static class ClusterPath
-{
-    public static string? AskForClusterPath()
-    {
-        while (true)
-        {
-            Console.Clear();
-            Console.WriteLine($"\n======== Kreator Klastrów ========\n");
-            Console.WriteLine($"Domyślna ścieżka: {RootPath.Value}\n");
-            Console.WriteLine(
-                "[1] Zastosuj\n" +
-                "[2] Ustaw inną\n" +
-                "[3] Anuluj\n");
+//namespace ArkServerCenter.Cluster;
+//using static MessageManager;
 
-            Console.Write("Wybierz: ");
-            string input = Console.ReadLine()?.Trim() ?? string.Empty;
 
-            if (input == "1")
-            {
-                Console.Clear();
-                Success("Zapisano ścieżkę!");
-                End(); return RootPath.Value;
-            }
+//public static class ClusterPath
+//{
+//    public static string? AskForClusterPath(string header)
+//    {
+//        while (true)
+//        {
+//            Console.Clear();
+//            Console.WriteLine(header);    
+//            Console.WriteLine($"Domyślna ścieżka: {RootPath.Value}\n");
+//            Console.WriteLine(
+//                "[1] Zastosuj\n" +
+//                "[2] Ustaw inną\n" +
+//                "[3] Anuluj\n");
 
-            else if (input == "2")
-            {
-                Console.Clear();
-                string? newPath = FileManager.AskForFolderPath("Ustawianie niestandardowej ścieżki.");
-                if (newPath == null) return null; // anuluj
+//            Console.Write("Wybierz: ");
+//            string input = Console.ReadLine()?.Trim() ?? string.Empty;
 
-                else
-                {
-                    Console.Clear();
-                    Success("Zapisano ścieżkę!");
-                    End(); return newPath;
-                }
-            }
+//            if (input == "1")
+//            {
+//                Console.Clear();
+//                Success("Zapisano ścieżkę!");
+//                End(); return RootPath.Value;
+//            }
 
-            else if (input == "3")
-            {
-                Console.Clear();
-                Console.WriteLine("Anulowano tworzenie klastra.");
-                End(); return null;
-            }
+//            else if (input == "2")
+//            {
+//                Console.Clear();
+//                string? newPath = FileManager.AskForFolderPath("Ustawianie niestandardowej ścieżki.");
+//                if (newPath == null) return null; // anuluj
 
-            else
-            {
-                Console.Clear();
-                Error("Nieprawidłowy wybór, spróbuj ponownie.");
-                End();
-            }
+//                else
+//                {
+//                    Console.Clear();
+//                    Success("Zapisano ścieżkę!");
+//                    End(); return newPath;
+//                }
+//            }
 
-        }
-    }
-}
+//            else if (input == "3")
+//            {
+//                Console.Clear();
+//                Console.WriteLine("Anulowano tworzenie klastra.");
+//                End(); return null;
+//            }
+
+//            else
+//            {
+//                Console.Clear();
+//                Error("Nieprawidłowy wybór, spróbuj ponownie.");
+//                End();
+//            }
+
+//        }
+//    }
+//}
