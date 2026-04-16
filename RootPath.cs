5912fd40be607c1d33111e58743607e59409f8ea
SIWY203 <cefalaspis12345@gmail.com>
2026-04-16 04:40:53 +0200

    - updated RootPath.Value eccessors - simplified TrySetPath() code - added ChangePath() - copied CopyDirectory() from BackupManager and modified for rootpath case

--- RootPath.cs
+++ RootPath.cs
@@ -9,20 +9,23 @@ public static class RootPath
 
     public static bool IsSet => !string.IsNullOrWhiteSpace(_value);
 
-    public static string Value => _value;
+    public static string Value
+    {
+        get => _value;
+        private set => _value = value;
+    }
+
 
     public static bool TrySetPath(string newPath)
     {
         if (string.IsNullOrWhiteSpace(newPath)) return false;
         string cleanPath = newPath.Trim().Replace("\"", "");
 
-        if (Directory.Exists(cleanPath))
-        {
-            _value = cleanPath;
-            Save();
-            return true;
-        }
-        return false;
+        if (!Directory.Exists(cleanPath)) return false;
+
+        _value = cleanPath;
+        Save();
+        return true;
     }
 
 
@@ -47,6 +50,33 @@ public static class RootPath
     }
 
 
+    public static void ChangePath()
+    {
+        Console.Clear();
+        string? newPath = FileManager.AskForFolderPath("Ustawianie głównego folderu.");
+        if (newPath == null) return; // anuluj
+        if (newPath == RootPath.Value)
+        {
+            Error("Wybrany folder jest już ustawiony jako domyślny! Brak zmian.");
+            End(); return;
+        }
+
+        bool success = CopyDirectory(RootPath.Value, newPath);
+        if (success)
+        {
+            RootPath.Value = newPath;
+            Save();
+
+            Success("Zapisano ścieżkę!");
+            while (Console.KeyAvailable) Console.ReadKey(intercept: true); // czyszczenie bufora klawiatury
+            End(); return;
+        }
+
+        Error("Nie udało się skopiować plików. Anulowano.");
+        End(); return;
+    }
+
+
     public static void Setup()
     {
         while (!IsSet)
@@ -65,6 +95,76 @@ public static class RootPath
                 End();
             }
         }
+    }
+
+
+
+    public static bool CopyDirectory(string sourceDir, string targetDir)
+    {
+        Console.Clear();
+
+        // sprawdzanie czy folder źródłowy istnieje
+        if (string.IsNullOrWhiteSpace(sourceDir) || !Directory.Exists(sourceDir))
+        {
+            Warn($"Folder źródłowy nie istnieje!\n{sourceDir}\n");
+            Log($"Jeśli pliki zostały przeniesione ręcznie, wszystko jest OK.");
+            Log($"Aktualizuję ścieżkę...");
+            return true;
+        }
+
+        // sprawdzanie czy folder docelowy nie jest pustym stringiem
+        if (string.IsNullOrWhiteSpace(targetDir))
+        {
+            Error("Ścieżka docelowa jest pusta!");
+            return false;
+        }
+
+        // bezpiecznik (Krytyczny przy rekurencji) zapobiega próbie kopiowania np. X do X\Kopia
+        string rel = Path.GetRelativePath(sourceDir, targetDir);
+        bool isSubfolder = !rel.StartsWith("..") && rel != ".";
+        if (isSubfolder)
+        {
+            Error("Nie możesz skopiować głównego folderu do jego własnego podfolderu!");
+            return false;
+        }
+        Log("Kopiowanie plików. Może to potrwać kilka minut...");
+
+        string tempDir = $"{targetDir}_temp";
+        try
+        {
+            // 1. czyszczenie temp
+            if (Directory.Exists(tempDir)) Directory.Delete(tempDir, true);
+            Directory.CreateDirectory(tempDir);
+
+            // 2. kopiowanie do temp
+            FileManager.CopyDirectoryCore(sourceDir, tempDir);
+
+            // 3. sprawdzenie czy aby folder nie jest pusty
+            if (Directory.GetFileSystemEntries(tempDir).Length == 0)
+            {
+                if (Directory.Exists(tempDir)) Directory.Delete(tempDir, true);
+                Error($"Folder tymczasowy jest pusty: \n{tempDir} \nOperacja przerwana.");
+                return false;
+            }
+
+            // 4. usuwanie folderu docelowego
+            if (Directory.Exists(targetDir)) Directory.Delete(targetDir, true);
+
+            // 5. przeniesienie do celu
+            Directory.Move(tempDir, targetDir);
+        }
+
+        catch (Exception ex)
+        {
+            Error($"{ex.Message}");
+            Console.WriteLine("Cofanie zmian... ");
+
+            // usuwanie uszkodzonych danych
+            if (Directory.Exists(tempDir)) Directory.Delete(tempDir, true);
+            return false;
+        }
+        return true;
+
     }
 
 
