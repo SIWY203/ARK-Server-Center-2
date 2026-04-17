b80c0c373d295e5de2bb2eb18649c9e73aa649bd
SIWY203 <cefalaspis12345@gmail.com>
2026-04-17 06:32:41 +0200

    - added global settings - moved RootPath to GlobalSettings namespace

---
+++ GlobalSettings/Address.cs
@@ -0,0 +1,48 @@
+﻿namespace ArkServerCenter.GlobalSettings;
+using System.Net;
+using static MessageManager;
+
+public class Address
+{
+	public static string IpAddress { get; set; } = "127.0.0.1";
+
+    public static void IpAddressMenu()
+	{
+        Console.WriteLine($"Aktualne IP: {IpAddress}");
+        Console.WriteLine("[1] Zmień Adres IP");
+        Console.WriteLine("[Q] Wróć");
+        string input = (Console.ReadLine()?.Trim() ?? "").ToUpper();
+        if (input == "1") ChangeIpAddress();
+    }
+
+	public static void ChangeIpAddress()
+	{
+		if (SafetyChecker.IsAnyServerRunning())
+		{
+			Error("Jeden z serwerów jest włączony! Anulowano.");
+			End(); return;
+		}
+
+        Console.WriteLine("========== Zmiana adresu IP ==========\n");
+        Console.WriteLine($"Aktualne IP: {IpAddress}");
+        Console.WriteLine($"Ustaw nowy adres, dla serwerów ARK\n");
+        Console.Write("Podaj: ");
+        string input = (Console.ReadLine()?.Trim() ?? "").ToUpper();
+            
+		if (string.IsNullOrWhiteSpace(input))
+		{
+            Error("Nie podano żadnej wartości!");
+			End(); return;
+		}
+
+		if (!IPAddress.IsValid(input))
+		{
+			Error("Nieprawidłowy adres IP!");
+			End(); return;
+		}
+
+		IpAddress = input;
+		Success($"Ustawiono nowe IP: {input}");
+		End(); return;
+	}
+}
---
+++ GlobalSettings/GlobalSettingsMenu.cs
@@ -0,0 +1,68 @@
+﻿namespace ArkServerCenter.GlobalSettings;
+using ArkServerCenter.Clusters;
+using static MessageManager;
+
+public class GlobalSettingsMenu
+{
+    public static void ShowMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("========= ARK SERVER CENTER =========");
+            Console.WriteLine("Globalne ustawienia\n");
+            Console.WriteLine("[1] Aktualizacja");
+            Console.WriteLine("[2] Adres IP");
+            Console.WriteLine("[3] Folder główny");
+            Console.WriteLine("[4] Skróty");
+            Console.WriteLine("[5] Język (language)");
+            Console.WriteLine("[Q] Wyjdź");
+            Console.Write("\nWybierz: ");
+
+            string? input = Console.ReadLine()?.ToUpper();
+            if (input == "Q")
+            {
+                Console.Clear();
+                ClusterManager.ActiveCluster = null;
+                break; // exit program
+            }
+
+            if (input == "1")
+            {
+                Console.Clear();
+                Updater.UpdateAllServers();
+                continue;
+            }
+
+            if (input == "2")
+            {
+                Console.Clear();
+                Address.IpAddressMenu();
+                continue;
+            }
+
+            if (input == "3")
+            {
+                Console.Clear();
+                RootPath.ChangePath();
+                continue;
+            }
+
+            if (input == "4")
+            {
+                Console.Clear();
+                Console.WriteLine("dostępne wkrótce..."); End();
+                continue;
+            }
+
+            if (input == "5")
+            {
+                Console.Clear();
+                Console.WriteLine("dostępne wkrótce..."); End();
+                continue;
+            }
+
+        }
+    }
+
+}
